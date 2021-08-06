e117a3102fa5aea28ee58c9bb646cf769bd87a75
daniel <villanod47@gmail.com>
2021-08-06 15:04:32 -0500

    error

---
+++ calculator.py
@@ -0,0 +1,10 @@
+print("")
+print("RESTA")
+print("")
+
+num1=int(input("Ingrese el primer digito: "))
+num2=int(input("Ingrese el segundo digito: "))
+
+resta=num1-num2
+
+print("La resta es igual a: ", resta)
